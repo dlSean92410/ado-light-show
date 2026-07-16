94b96956f7c11e486d6e4148ed1335766d7223cf
Sean Wen <sw92410@hotmail.com>
2026-07-16 21:04:44 +0900

    clean up content script, move device control logic to penlight-manager page

---
+++ apps/extension/src/content/index.ts
@@ -0,0 +1,29 @@
+import type { Message, MessageResponse } from '@/utility/type';
+import VideoController from '@/content/VideoController';
+
+(async () => {
+	console.debug('Ado Light Show extension running...');
+
+	const videoController = new VideoController();
+
+	chrome.runtime.onMessage.addListener((message: Message, _sender, sendResponse) => {
+		console.debug('Ado Light Show extension content - onMessage', { message });
+
+		switch (message.type) {
+			case 'GET_VIDEO_TITLE': {
+				const title = videoController.getTitle();
+				const response: MessageResponse<'GET_VIDEO_TITLE'> = { value: title };
+				sendResponse(response);
+				break;
+			}
+			case 'GET_VIDEO_TIME': {
+				const time = videoController.getVideo()?.currentTime ?? -1;
+				const response: MessageResponse<'GET_VIDEO_TIME'> = { value: time };
+				sendResponse(response);
+				break;
+			}
+			default:
+				break;
+		}
+	});
+})();
