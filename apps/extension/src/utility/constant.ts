8c5f5e17745fd5b988efb0f7200424515f042245
Sean Wen <sw92410@hotmail.com>
2026-07-16 21:07:42 +0900

    refactor util and style

--- apps/extension/src/utility/constant.ts
+++ apps/extension/src/utility/constant.ts
@@ -2,9 +2,10 @@ import type { ScriptSource } from './type';
 
 const DEVICE_NAME = 'Ado Light Stick';
 const SERVICE_UUID = '00000000-0000-1000-8000-00805f9b34fb';
+const PENLIGHT_MANAGER_PATH = 'src/page/penlight-manager/index.html';
 
 const REMOTE_SCRIPT_BASE_URL =
 	'https://raw.githubusercontent.com/dlSean92410/ado-light-show/main/scripts';
 const SCRIPT_SOURCES: [ScriptSource, ScriptSource] = ['CUSTOM', 'REMOTE'];
 
-export { DEVICE_NAME, SERVICE_UUID, REMOTE_SCRIPT_BASE_URL, SCRIPT_SOURCES };
+export { DEVICE_NAME, SERVICE_UUID, PENLIGHT_MANAGER_PATH, REMOTE_SCRIPT_BASE_URL, SCRIPT_SOURCES };
